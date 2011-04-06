8ac6c2dc3daee3f93812feec60e3d9dfb27a0405
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-06 20:23:03 +0200

    Add method for sending UDP packet to StatsD

--- src/main/scala/no/kvikshaug/StatsD.scala
+++ src/main/scala/no/kvikshaug/StatsD.scala
@@ -3,6 +3,15 @@ package no.kvikshaug
 import java.net._
 
 object StatsD {
+  // TODO load from config
+  val host = InetAddress.getByAddress(Array(127, 0, 0, 1))
+  val port = 2003
 
+  def send(data: String, sampleRate: Double = 1) {
+    val socket = new DatagramSocket(port)
+    val payload = data.getBytes
+    val packet = new DatagramPacket(payload, payload.length, host, port)
+    socket.send(packet)
+  }
 }
 
