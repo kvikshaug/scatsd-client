8aae42b984795508d1503962853f54c9c958795c
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-08 10:13:25 +0200

    Take samples only at sample rate

--- src/main/scala/no/kvikshaug/StatsD.scala
+++ src/main/scala/no/kvikshaug/StatsD.scala
@@ -7,7 +7,12 @@ object StatsD {
   val host = InetAddress.getByAddress(Array(127, 0, 0, 1))
   val port = 2003
 
-  def send(data: String, sampleRate: Double = 1) {
+  val random = new java.util.Random
+
+  def send(data: String, sampleRate: Double = 1): Unit = {
+    if(sampleRate < 1 && sampleRate < random.nextDouble) {
+      return
+    }
     val socket = new DatagramSocket(port)
     val payload = data.getBytes
     val packet = new DatagramPacket(payload, payload.length, host, port)
